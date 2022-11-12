a91e80bb7205ad8114f6cc324d02ca58ab62d623
GatsbyJr <61660956+goclfc@users.noreply.github.com>
2022-11-12 20:04:13 +0400

    server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,26 @@
-const express = require('express')
-const app=express()
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const xss = require('xss-clean');
+const mongoSanitize = require('express-mongo-sanitize');
+const routes = require('./routes')
+
+/// body parse
+app.use(express.json())
+
+
+/// sanitize
+app.use(xss());
+app.use(mongoSanitize());
+
+/// routes
+app.use('/api',routes)
+
+
+
 
 
 const port = process.env.PORT || 3001
 app.listen(port,()=>{
-    console.log('server is runing on port:',port)
-})
+    console.log(`Server is running on port ${port}`)
+});
