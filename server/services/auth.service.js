462358bb81836139e73088e060044b45dd0a89a1
GatsbyJr <61660956+goclfc@users.noreply.github.com>
2022-11-12 20:20:04 +0400

    update auth service

--- server/services/auth.service.js
+++ server/services/auth.service.js
@@ -1,23 +1,23 @@
-const hello = async(req,res) => {
-    const csvFilePath='./server/db.csv'
-const csv=require('csvtojson')
-csv()
-.fromFile(csvFilePath)
-.then((jsonObj)=>{
-    console.log(jsonObj,'this');
-    /**
-     * [
-     * 	{a:"1", b:"2", c:"3"},
-     * 	{a:"4", b:"5". c:"6"}
-     * ]
-     */ 
-})
- 
-// Async / await usage
-try{
-        const jsonArray=await csv().fromFile(csvFilePath);
-        res.json(jsonArray) ;
-    } catch(error){
+const hello = async (req, res) => {
+    const csvFilePath = './server/db.csv'
+    const csv = require('csvtojson')
+    csv()
+        .fromFile(csvFilePath)
+        .then((jsonObj) => {
+            console.log(jsonObj, 'this');
+            /**
+             * [
+             * 	{a:"1", b:"2", c:"3"},
+             * 	{a:"4", b:"5". c:"6"}
+             * ]
+             */
+        })
+
+    // Async / await usage
+    try {
+        const jsonArray = await csv().fromFile(csvFilePath);
+        res.json(jsonArray);
+    } catch (error) {
 
     }
 }
