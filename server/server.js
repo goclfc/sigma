9db6c6143870218aec9b0938ec46cc4215c19b4e
GatsbyJr <61660956+goclfc@users.noreply.github.com>
2022-11-12 20:29:52 +0400

    add

--- server/server.js
+++ server/server.js
@@ -4,11 +4,11 @@ const mongoose = require('mongoose');
 const xss = require('xss-clean');
 const mongoSanitize = require('express-mongo-sanitize');
 const routes = require('./routes')
-
+const cors = require('cors');
 /// body parse
 app.use(express.json())
 
-
+app.use(cors());
 /// sanitize
 app.use(xss());
 app.use(mongoSanitize());
@@ -16,8 +16,12 @@ app.use(mongoSanitize());
 /// routes
 app.use('/api',routes)
 
-
-
+const ads = [
+    {title: 'Hello, world (again)!'}
+  ];
+  app.get('/', (req, res) => {
+    res.send(ads);
+  });
 
 
 const port = process.env.PORT || 3001
